3730f76e53bef61310e14bd87815ec0c4e2f8369
Herysia <totogirerd@gmail.com>
2021-01-31 13:48:50 +0100

    Initial commit

---
+++ HideGameCode/HideGameCodePlugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Reactor;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace HideGameCode
+{
+    [BepInPlugin(Id)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class HideGameCodePlugin : BasePlugin
+    {
+        public const string Id = "com.herysia.hidegamecode";
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+
+        public override void Load()
+        {
+            Harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(GameStartManager), nameof(GameStartManager.Start))]
+        public static class GameStartManager_Start
+        {
+            public static void copyToClipboard()
+            {
+                GUIUtility.systemCopyBuffer = GameCode.IntToGameName(AmongUsClient.Instance.GameId);
+            }
+            public static void Postfix(GameStartManager __instance)
+            {
+                __instance.GameRoomName.Text = "TWITCH\r\nPRIME";
+                copyToClipboard();
+                var btn = __instance.MakePublicButton.GetComponent<PassiveButton>();
+                btn.OnClick.AddListener((UnityAction)copyToClipboard);
+            }
+        }
+    }
+}
